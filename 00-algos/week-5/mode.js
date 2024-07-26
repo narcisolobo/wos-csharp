f70fe6319130e36567f34b174ea7b5ce9658bb70
narcisolobo <narcisolobo@gmail.com>
2024-07-26 08:24:45 -0700

    w5d5 algo solution

--- 00-algos/week-5/mode.js
+++ 00-algos/week-5/mode.js
@@ -4,7 +4,7 @@
   Create a function that, given an array of ints,
   returns the int that occurs most frequently in the array.
 
-  What if there are multiple items that occur the same number of time?
+  What if there are multiple items that occur the same number of times?
     - return all of them (in an array)
     - what if all items occur the same number of times?
       - return empty array
@@ -35,5 +35,37 @@ const expected5 = [5, 1];
  * @returns {Array<number>} Mode or modes in any order.
  */
 function mode(nums) {
-  // your code here
+  if (nums.length === 1) {
+    return [nums[0]];
+  }
+
+  const modes = [];
+  const freqTable = {};
+  let maxFreq = 0;
+  let allSameFreq = true;
+
+  for (const n of nums) {
+    freqTable.hasOwnProperty(n) ? freqTable[n]++ : (freqTable[n] = 1);
+
+    if (freqTable[n] > maxFreq) {
+      maxFreq = freqTable[n];
+    }
+  }
+
+  for (const key in freqTable) {
+    if (freqTable[key] === maxFreq) {
+      // Keys are strings, convert back to int. Can be avoided if using a Map.
+      modes.push(parseInt(key));
+    } else {
+      allSameFreq = false;
+    }
+  }
+  // return empty array if allSameFreq, else return modes
+  return allSameFreq ? [] : modes;
 }
+
+console.log(mode(nums1));
+console.log(mode(nums2));
+console.log(mode(nums3));
+console.log(mode(nums4));
+console.log(mode(nums5));
