f740636fa37f049af6b383eac58250ba22b2af14
narcisolobo <narcisolobo@gmail.com>
2024-07-31 06:58:47 -0700

    w6d3 algo prep

---
+++ 00-algos/week-6-recursion/reverse-string.js
@@ -0,0 +1,24 @@
+/* 
+  Recursively reverse a string
+  helpful methods:
+
+  str.slice(beginIndex[, endIndex])
+  returns a new string from beginIndex to endIndex exclusive
+*/
+
+const str1 = 'abc';
+const expected1 = 'cba';
+
+const str2 = '';
+const expected2 = '';
+
+/**
+ * Recursively reverses a string.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {string} The given str reversed.
+ */
+function reverseStr(str) {
+  // your code here
+}
