b9dfcd4a65fbbc3afe2c6e3e8ae213f035e1eca9
narcisolobo <narcisolobo@gmail.com>
2024-08-12 10:04:55 -0700

    w8d1 algo solution

--- 00-algos/week-7-sorts/merge-sort.py
+++ 00-algos/week-7-sorts/merge-sort.py
@@ -27,26 +27,29 @@ def merge(left, right):
     Returns:
     list of int: A new sorted list containing all the elements of both given halves.
     """
+    merged = []
+    left_index, right_index = 0, 0
 
     # Continue to merge until one of the lists is exhausted
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
     # If there are remaining elements in the left list, add them to the merged list
+    while left_index < len(left):
+        merged.append(left[left_index])
+        left_index += 1
+
     # If there are remaining elements in the right list, add them to the merged list
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index += 1
 
-
-def merge_sort(lst):
-    """
-    Sorts the provided list using the merge sort algorithm.
-
-    Time: O(n log n)
-    Space: O(n)
-
-    Args:
-    arr (list of int): The list to sort.
-
-    Returns:
-    list of int: A new sorted list.
-    """
-    # your code here
+    return merged
 
 
 # Test cases
@@ -66,7 +69,37 @@ sortedA4 = [1, 2, 4, 5, 6, 9]
 sortedB4 = [3, 7, 8, 10]
 expectedMerge4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-print(merge(sortedA1, sortedB1))  # []
-print(merge(sortedA2, sortedB2))  # [2, 5]
-print(merge(sortedA3, sortedB3))  # [2, 3, 3, 4, 7]
-print(merge(sortedA4, sortedB4))  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(merge(sortedA1, sortedB1))
+print(merge(sortedA2, sortedB2))
+print(merge(sortedA3, sortedB3))
+print(merge(sortedA4, sortedB4))
+
+
+def merge_sort(lst):
+    """
+    Sorts the provided list using the merge sort algorithm.
+
+    Time: O(n log n)
+    Space: O(n)
+
+    Args:
+    lst (list of int): The list to sort.
+
+    Returns:
+    list of int: A new sorted list.
+    """
+    if len(lst) <= 1:
+        return lst
+
+    mid = len(lst) // 2
+    left_half = merge_sort(lst[:mid])
+    right_half = merge_sort(lst[mid:])
+
+    return merge(left_half, right_half)
+
+
+# Test cases
+numbersOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbersRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]
+numbersReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+expectedSort = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
