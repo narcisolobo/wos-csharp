389081d440bf86f7c789e821b0eccae1a26ad56a
narcisolobo <narcisolobo@gmail.com>
2024-08-14 07:29:41 -0700

    w8d3 algo solution

--- 00-algos/week_7_sorts/quick_sort.py
+++ 00-algos/week_7_sorts/quick_sort.py
@@ -1,5 +1,3 @@
-from week_7_sorts.partition import partition
-
 numbers1 = [11, 8, 14, 3, 6, 2, 7]
 expected1 = [2, 3, 6, 7, 8, 11, 14]
 
@@ -11,6 +9,40 @@ expected3 = [2, 3, 3, 3, 6, 7, 8, 11, 14]
 
 numbers4 = [1, 17, 12, 3, 9, 13, 21, 4, 27]
 expected4 = [1, 3, 4, 9, 12, 13, 17, 21, 27]
+
+
+def partition(numbers, left_idx=0, right_idx=None):
+    """
+    Partitions the given array in-place by selecting the number at the middle
+    index to use it as a "pivot" value, then arranges all numbers less than the
+    pivot to be on its left and all larger numbers to be on its right.
+
+    - Time: O(n) where n is the number of elements between left_idx and right_idx.
+    - Space: O(1), as the partitioning is done in-place.
+
+    Args:
+        numbers (list of int): The array of numbers to be partitioned.
+        left_idx (int): The index indicating the start of the slice of the array
+            being processed. Defaults to 0.
+        right_idx (int): The index indicating the end of the slice of the array
+            being processed. Defaults to None, which is set to the last index.
+
+    Returns:
+        int: The index where the left section of smaller items ends.
+    """
+    if right_idx is None:
+        right_idx = len(numbers) - 1
+
+    pivot_value = numbers[right_idx]
+    pivot_idx = left_idx
+
+    for i in range(left_idx, right_idx):
+        if numbers[i] <= pivot_value:
+            numbers[i], numbers[pivot_idx] = numbers[pivot_idx], numbers[i]
+            pivot_idx += 1
+
+    [numbers[pivot_idx], numbers[right_idx]] = [numbers[right_idx], numbers[pivot_idx]]
+    return pivot_idx
 
 
 def quick_sort(numbers, left_idx=0, right_idx=None):
@@ -30,3 +62,16 @@ def quick_sort(numbers, left_idx=0, right_idx=None):
     """
     if right_idx is None:
         right_idx = len(numbers) - 1
+
+    if left_idx < right_idx:
+        pivot_index = partition(numbers, left_idx, right_idx)
+        quick_sort(numbers, left_idx, pivot_index - 1)
+        quick_sort(numbers, pivot_index + 1, right_idx)
+
+    return numbers
+
+
+print(quick_sort(numbers1))
+print(quick_sort(numbers2))
+print(quick_sort(numbers3))
+print(quick_sort(numbers4))
