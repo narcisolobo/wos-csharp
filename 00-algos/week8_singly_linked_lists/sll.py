ca9bca8d14a7815287d815b5f03f95001763447f
narcisolobo <narcisolobo@gmail.com>
2024-08-15 08:35:56 -0700

    w8d4 algo solutions

--- 00-algos/week8_singly_linked_lists/sll.py
+++ 00-algos/week8_singly_linked_lists/sll.py
@@ -1,3 +1,6 @@
+from typing import Any, Self
+
+
 class ListNode:
     """
     A class to represent a single item of a SinglyLinkedList that can be
@@ -5,7 +8,7 @@ class ListNode:
     """
 
     # (dunder init/constructor)
-    def __init__(self, data):
+    def __init__(self, data: int):
         """
         Constructs a new Node instance.
         :param data: The data to be added into this new instance of a Node.
@@ -14,7 +17,7 @@ class ListNode:
         self.next = None
 
     def __str__(self):
-        return f"data: {self.data}, next: {self.next}"
+        return f"{self.data} -> {self.next}"
 
 
 class SinglyLinkedList:
@@ -32,14 +35,14 @@ class SinglyLinkedList:
     def __str__(self):
         return f"head: {self.head}"
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """
         Determines if this list is empty.
             :returns: bool - True if the list is empty, False otherwise.
         """
-        pass
+        return self.head is None
 
-    def insert_at_back(self, data):
+    def insert_at_back(self, data: int) -> Self:
         """
         Creates a new node with the given data and inserts it at the back of
         this list.
@@ -47,23 +50,55 @@ class SinglyLinkedList:
             :returns: SinglyLinkedList - This list.
         """
         # create a new ListNode
+        new_node = ListNode(data)
+
         # check if SLL is empty
         # if so, set head to new ListNode
+        if self.is_empty():
+            self.head = new_node
+
         # if not, we need to find the last node
         # we create a runner, set it to self.head
-        # while runner.next is not None
-        # runner = runner.next (i++)
-        # after the while loop, runner.next = new_node
-        pass
+        else:
+            runner = self.head
+            # while runner.next is not None
+            while runner.next:
+                # increment the runner (i++)
+                runner = runner.next
+            # after the while loop, runner.next = new_node
+            runner.next = new_node
 
-    def to_list(self):
+        # this is for chaining methods
+        return self
+
+    def insert_at_back_many(self, vals: list[int]) -> Self:
+        """
+        Calls insert_at_back on each item of the given list.
+            :param vals: List of values to be added to the list.
+            :returns: SinglyLinkedList - This list.
+        """
+        for item in vals:
+            self.insert_at_back(item)
+        return self
+
+    def to_list(self) -> list[Any]:
         """
         Converts this list into a list containing the data of each node.
             :returns: List - A list of each node's data.
         """
-        pass
+        lst = []
+        runner = self.head
+        while runner:
+            lst.append(runner.data)
+            runner = runner.next
+        return lst
 
 
-my_sll = SinglyLinkedList()
+# Test case
+my_sll = SinglyLinkedList().insert_at_back_many([5, 10, 4, 3, 6, 1, 7, 2])
+
+# Convert the singly linked list to a list and print it
+print(my_sll.to_list())
+
+# Print the singly linked list with the __str__ method
 print(my_sll)
-my_sll.insert_at_back(5)
